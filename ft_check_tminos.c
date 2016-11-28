ea0445de96ddf5f7f929d1f3ed8cbf6ba013b3c2
Aymeric JEHANNO <ajehanno@e2r7p5.42.fr>
2016-11-28 16:55:48 +0100

    Push of ft_check_tminos.c

---
+++ ft_check_tminos.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_tminos.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/26 14:57:19 by mfranc            #+#    #+#             */
+/*   Updated: 2016/11/28 16:48:43 by ajehanno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int		ft_check_tminos(char *str)
+{
+	int r;
+	int o;
+
+	F * t[] = {ft_check_tminos1, ft_check_tminos2, ft_check_tminos3, 
+	ft_check_tminos4, ft_check_tminos5, ft_check_tminos6, ft_check_tminos7, 
+	ft_check_tminos8, ft_check_tminos9, ft_check_tminos10, ft_check_tminos11, 
+	ft_check_tminos12, ft_check_tminos13, ft_check_tminos14,
+	ft_check_tminos15, ft_check_tminos16, ft_check_tminos17, 
+	ft_check_tminos18, ft_check_tminos19}; 
+	r = 0;
+	o = 0;
+	while (o < 19)
+	{
+		if (t[o](str, i) > r)
+			r = t[o](str, i);
+		o++;
+	}
+	ft_putnbr(r);
+	return (r);
+}
