728f5faf2f34c34af2e7aa5b16ee73b43ed8ede9
Maxime FRANC <mfranc@e2r6p6.42.fr>
2016-11-28 20:41:52 +0100

    last modif and test on check and check map, just top visualize our futur script on backtrack. s'ye soon

--- ft_get_tminos_1_5.c
+++ ft_get_tminos_1_5.c
@@ -1,34 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_check.c                                         :+:      :+:    :+:   */
+/*   ft_get_tminos_1_5.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/26 18:02:55 by mfranc            #+#    #+#             */
-/*   Updated: 2016/11/28 20:40:26 by mfranc           ###   ########.fr       */
+/*   Created: 2016/11/28 19:23:45 by mfranc            #+#    #+#             */
+/*   Updated: 2016/11/28 19:27:54 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		*ft_check(int fd)
+char	*ft_get_tminos_1(int i)
 {
-	char			*file;
-	int				*tminos;
-	int				nb_map;
+	if (i)
+		ft_putendl("##\n##");
+}
 
-	if (fd == - 1)
-		return (0);
-	file = ft_get_file(fd);
-	if (ft_check_file(file) == 0)
-		return (0);
-	nb_map = ft_get_map(file);
-	if (nb_map == 0)
-		return (0);
-	tminos = ft_check_map(file);
-	if (tminos == 0)
-		return (0);
-	ft_settab(nb_map, tminos);
-	return (1);
+char	*ft_get_tminos_2(int i)
+{
+	if (i)
+		ft_putendl("#\n#\n#\n#");
 }
