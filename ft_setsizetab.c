0b29428e95dcd8242fd09790f557b606076428b4
Maxime FRANC <mfranc@e2r6p7.42.fr>
2016-11-30 17:02:55 +0100

    finish setsizetab ! and fix conflicts on headerand main

--- ft_setsizetab.c
+++ ft_setsizetab.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 16:22:34 by mfranc            #+#    #+#             */
-/*   Updated: 2016/11/30 15:13:03 by ajehanno         ###   ########.fr       */
+/*   Updated: 2016/11/30 16:54:26 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,24 @@
 
 int	ft_set_size_tab(int nb_map)
 {
-	int tab[27];
-
-	tab = {NULL, 2, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 
-	10, 10, 10, 10, 11, 11};
-	return (tab[nb_map]);
+	if (nb_map == 1 || nb_map == 2 || nb_map == 3)
+		return (nb_map + 1);
+	else if (nb_map == 4 || nb_map == 5)
+		return (nb_map);
+	else if (nb_map == 6)
+		return (nb_map - 1);
+	else if (nb_map >= 7 && nb_map <= 9)
+		return (6);
+	else if (nb_map >= 10 && nb_map <= 12)
+		return (7);
+	else if (nb_map >= 13 && nb_map <= 16)
+		return (8);
+	else if (nb_map >= 17 && nb_map <= 20)
+		return (9);
+	else if (nb_map >= 21 && nb_map <= 24)
+		return (10);
+	else if (nb_map >= 25 && nb_map <= 27)
+		return (11);
+	else
+		return (0);
 }
