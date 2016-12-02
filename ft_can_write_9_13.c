8c5ea5efc00200ff63895253988298adc4961e35
Maxime FRANC <mfranc@e2r5p7.42.fr>
2016-12-02 14:48:50 +0100

    modify the variable on ft_can write to zork on address of structure preciselly

--- ft_can_write_9_13.c
+++ ft_can_write_9_13.c
@@ -2,65 +2,80 @@
 
 int	ft_can_write9(int o, t_place *****p)
 {
-	if (p->tab[p->y - 1][p->x] != '.' 
-			|| cp->tab[p->y - 2][p->x] != '.' 
-			|| cp->tab[p->y - 2][p->x + 1] != '.')
+	t_place	*cp;
+
+	cp = &p;
+	if (cp->tab[cp->y - 1][cp->x] != '.' 
+			|| cp->tab[cp->y - 2][cp->x] != '.' 
+			|| cp->tab[cp->y - 2][cp->x + 1] != '.')
 		return (0);
-	cp->tab[p->y][p->x] = 'A' + o;
-	cp->tab[p->y - 1][cp->x] = 'A' + o;
-	cp->tab[p->y - 2][p->x] = 'A' + o;
-	cp->tab[p->y - 2][p->x + 1] = 'A' + o;
+	cp->tab[cp->y][p->x] = 'A' + o;
+	cp->tab[cp->y - 1][cp->x] = 'A' + o;
+	cp->tab[cp->y - 2][cp->x] = 'A' + o;
+	cp->tab[cp->y - 2][cp->x + 1] = 'A' + o;
 	return (1);
 }
 
 int	ft_can_write10(int o, t_place *****p)
 {
-	if (p->tab[p->y - 1][p->x] != '.' 
-			|| p->tab[p->y][p->x + 1] != '.' 
-			|| p->tab[p->y][p->x + 2] != '.')
+	t_place	*cp;
+
+	cp = &p;
+	if (cp->tab[cp->y - 1][cp->x] != '.' 
+			|| cp->tab[cp->y][cp->x + 1] != '.' 
+			|| cp->tab[cp->y][cp->x + 2] != '.')
 		return (0);
-	cp->tab[p->y][p->x] = 'A' + o;
-	cp->tab[p->y - 1][cp->x] = 'A' + o;
-	cp->tab[p->y][p->x + 1] = 'A' + o;
-	cp->tab[p->y][p->x + 2] = 'A' + o;
+	cp->tab[cp->y][cp->x] = 'A' + o;
+	cp->tab[cp->y - 1][cp->x] = 'A' + o;
+	cp->tab[cp->y][cp->x + 1] = 'A' + o;
+	cp->tab[cp->y][cp->x + 2] = 'A' + o;
 	return (1);
 }
 
 int	ft_can_write11(int o, t_place *****p)
 {
-	if (cp->tab[p->y - 1][p->x] != '.' 
-			|| p->tab[p->y - 1][p->x + 1] != '.' 
-			|| p->tab[p->y - 1][p->x + 2] != '.')
+	t_place	*cp;
+
+	cp = &p;
+	if (cp->tab[cp->y - 1][cp->x] != '.' 
+			|| cp->tab[cp->y - 1][cp->x + 1] != '.' 
+			|| cp->tab[cp->y - 1][cp->x + 2] != '.')
 		return (0);
-	p->tab[p->y][p->x] = 'A' + o;
-	p->tab[p->y - 1][p->x] = 'A' + o;
-	p->tab[p->y - 1][p->x + 1] = 'A' + o;
-	p->tab[p->y - 1][p->x + 2] = 'A' + o;
+	p->tab[cp->y][cp->x] = 'A' + o;
+	p->tab[cp->y - 1][cp->x] = 'A' + o;
+	p->tab[cp->y - 1][cp->x + 1] = 'A' + o;
+	p->tab[cp->y - 1][cp->x + 2] = 'A' + o;
 	return (1);
 }
 
 int	ft_can_write12(int o, t_place *****p)
 {
-	if (cp->tab[p->y][p->x + 1] != '.' 
-			|| p->tab[p->y - 1][p->x + 1] != '.' 
-			|| p->tab[p->y - 1][p->x + 2] != '.')
+	t_place	*cp;
+
+	cp = &p;
+	if (cp->tab[cp->y][cp->x + 1] != '.' 
+			|| cp->tab[cp->y - 1][cp->x + 1] != '.' 
+			|| cp->tab[cp->y - 1][cp->x + 2] != '.')
 		return (0);
-	p->tab[p->y][p->x] = 'A' + o;
-	p->tab[p->y][p->x + 1] = 'A' + o;
-	p->tab[p->y - 1][p->x + 1] = 'A' + o;
-	p->tab[p->y - 1][p->x + 2] = 'A' + o;
+	p->tab[cp->y][cp->x] = 'A' + o;
+	p->tab[cp->y][cp->x + 1] = 'A' + o;
+	p->tab[cp->y - 1][cp->x + 1] = 'A' + o;
+	p->tab[cp->y - 1][cp->x + 2] = 'A' + o;
 	return (1);
 }
 
 int	ft_can_write13(int o, t_place *****p)
 {
-	if (cp->tab[p->y - 1][p->x] != '.' 
-			|| p->tab[p->y - 1][p->x - 1] != '.' 
-			|| p->tab[p->y - 2][p->x - 1] != '.')
+	t_place	*cp;
+
+	cp = &p;
+	if (cp->tab[cp->y - 1][cp->x] != '.' 
+			|| cp->tab[cp->y - 1][cp->x - 1] != '.' 
+			|| cp->tab[cp->y - 2][cp->x - 1] != '.')
 		return (0);
-	p->tab[p->y][p->x] = 'A' + o;
-	p->tab[p->y - 1][p->x] = 'A' + o;
-	p->tab[p->y - 1][p->x - 1] = 'A' + o;
-	p->tab[p->y - 2][p->x - 1] = 'A' + o;
+	p->tab[cp->y][cp->x] = 'A' + o;
+	p->tab[cp->y - 1][cp->x] = 'A' + o;
+	p->tab[cp->y - 1][cp->x - 1] = 'A' + o;
+	p->tab[cp->y - 2][cp->x - 1] = 'A' + o;
 	return (1);
 }
